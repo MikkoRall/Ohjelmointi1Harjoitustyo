37d9b4379e7e4ea01420e592bc21bd834fa6e9b7
Ralli Mikko Juhani <mikko.j.ralli@student.jyu.fi>
2024-02-10 18:23:38 +0200

    Harjoitustyön pohja

---
+++ suunnitelma/Harjoitustyö/Matopeli2024/Matopeli2024.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Widgets;
+
+namespace Matopeli2024;
+
+/// @author Omanimi
+/// @version 10.02.2024
+/// <summary>
+/// 
+/// </summary>
+public class Matopeli2024 : PhysicsGame
+{
+    public override void Begin()
+    {
+        // Kirjoita ohjelmakoodisi tähän
+
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+}
---
+++ suunnitelma/Harjoitustyö/Matopeli2024/Ohjelma.cs
@@ -0,0 +1,26 @@
+﻿#region Using Statements
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace Matopeli2024
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using var game = new Matopeli2024();
+            game.Run();
+        }
+    }
+}
